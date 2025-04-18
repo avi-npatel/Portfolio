877dd5d3b7c1dfec3a9388c1e40d52c0c00bd2b6
avipatelchapelhill <avipatel.chapelhill@gmail.com>
2025-04-18 18:06:18 -0400

    updated about me page with passions and other section

--- portfolio-master/assets/js/script.js
+++ portfolio-master/assets/js/script.js
@@ -10,8 +10,8 @@ const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 // sidebar toggle functionality for mobile
 sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
 
-// testimonials variables
-const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
+// Passions variables
+const PassionsItem = document.querySelectorAll("[data-Passions-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
 const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
 const overlay = document.querySelector("[data-overlay]");
@@ -22,26 +22,26 @@ const modalTitle = document.querySelector("[data-modal-title]");
 const modalText = document.querySelector("[data-modal-text]");
 
 // modal toggle function
-const testimonialsModalFunc = function () {
+const PassionsModalFunc = function () {
   modalContainer.classList.toggle("active");
   overlay.classList.toggle("active");
 }
 
 // add click event to all modal items
-for (let i = 0; i < testimonialsItem.length; i++) {
-  testimonialsItem[i].addEventListener("click", function () {
-    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
-    modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
-    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
-    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
-    testimonialsModalFunc();
+for (let i = 0; i < PassionsItem.length; i++) {
+  PassionsItem[i].addEventListener("click", function () {
+    modalImg.src = this.querySelector("[data-Passions-avatar]").src;
+    modalImg.alt = this.querySelector("[data-Passions-avatar]").alt;
+    modalTitle.innerHTML = this.querySelector("[data-Passions-title]").innerHTML;
+    modalText.innerHTML = this.querySelector("[data-Passions-text]").innerHTML;
+    PassionsModalFunc();
   });
 
 }
 
 // add click event to modal close button
-modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-overlay.addEventListener("click", testimonialsModalFunc);
+modalCloseBtn.addEventListener("click", PassionsModalFunc);
+overlay.addEventListener("click", PassionsModalFunc);
 
 // custom select variables
 const select = document.querySelector("[data-select]");
